2c510a63c71d13bb651f1ee8c8a507835838f474
VadymKarko <vadym.karko@gmail.com>
2015-05-18 08:30:18 -0400

    Created domains for Hospital

---
+++ domain/src/main/java/projectx/domain/Request.java
@@ -0,0 +1,74 @@
+package projectx.domain;
+
+import java.util.Date;
+
+/**
+ * @author proger
+ * @since 5/18/15
+ */
+public class Request {
+
+    private String firstName;
+    private String fatherName;
+    private String lastName;
+    private String illnessDescription;
+    private String symptoms;
+    private String phoneNumber;
+    private Date preferredDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIllnessDescription() {
+        return illnessDescription;
+    }
+
+    public void setIllnessDescription(String illnessDescription) {
+        this.illnessDescription = illnessDescription;
+    }
+
+    public String getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(String symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getPreferredDate() {
+        return preferredDate;
+    }
+
+    public void setPreferredDate(Date preferredDate) {
+        this.preferredDate = preferredDate;
+    }
+}
