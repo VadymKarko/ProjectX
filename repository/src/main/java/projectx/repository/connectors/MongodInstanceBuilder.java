2482099fedb1abe9f4965bddf3e6ead10692c600
VadymKarko <vadym.karko@gmail.com>
2015-05-25 16:27:58 -0400

    Mongod Process builder

--- repository/src/main/java/projectx/repository/connectors/MongodInstanceBuilder.java
+++ repository/src/main/java/projectx/repository/connectors/MongodInstanceBuilder.java
@@ -1,6 +1,8 @@
 package projectx.repository.connectors;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author proger
@@ -8,49 +10,64 @@ import java.io.IOException;
  */
 public class MongodInstanceBuilder {
     private static String dbpath;
+    private static int nodePort0 = 27018;
+    private static int nodePort1 = 27019;
+    private static int nodePort2 = 27020;
+    private static Map<Integer, Process> mongodMap = new HashMap<Integer, Process>();
+    private static MongodInstanceBuilder instance;
 
-    public static Process runMongodInstance(Integer port) throws IOException, InterruptedException {
-        String line;
-        switch(port){
-            case 27018:{
-                dbpath = "/rs0-0";
-                break;
-            }
-            case 27019:{
-                dbpath = "/rs0-1";
-                break;
-            }
-            case 27020:{
-                dbpath = "/rs0-2";
-                break;
-            }
-            default:{
-                return null;
-            }
+    public static MongodInstanceBuilder getInstance(){
+        if (instance == null){instance = new MongodInstanceBuilder();}
+        return instance;
+    }
+    public  void runMongod(int port) throws IOException,
+            InterruptedException, IllegalArgumentException {
+        if (port != nodePort0 && port != nodePort1 && port != nodePort2){
+            throw  new IllegalArgumentException("Invalid port");
+        }
+        switch(port){case 27018:{dbpath = "/rs0-0";
+                                break;}
+                     case 27019:{dbpath = "/rs0-1";
+                                break;}
+                     case 27020:{dbpath = "/rs0-2";
+                                break;} }
+
+        for (int i=0;i<mongodMap.size();i++)
+        {
+            if (mongodMap.keySet().toArray()[i].equals(port)){ stopMongod(port); }
         }
         String[] command = {"bash","-c","echo proger| sudo -S mongod --port "
-                + port.toString() + " --dbpath " + dbpath + " --replSet rs0 "};
+                + port + " --dbpath " + dbpath + " --replSet rs0 "};
         Process process = Runtime.getRuntime().exec(command);
-       // BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        //while ((line = input.readLine()) != null) {
-          //  System.out.println(line);
-       // }
-        //input.close();
-        return process;
+        mongodMap.put(port,process);
     }
 
+    public static Process getProcess(int port){
+        return mongodMap.get(port);
+    }
 
+    public  void stopMongod(int port){
+        getProcess(port).destroy();
+        mongodMap.remove(port);
+
+    }
+    public  void showMapEntries(){
+        System.out.println(mongodMap.entrySet());
+    }
 
 /*
+
     public static void main(String[] args)
-            throws IOException, InterruptedException
+            throws IOException, InterruptedException, IllegalArgumentException
     {
-       Process p = runMongodInstance(27018);
-        Process p1 = runMongodInstance(27019);
-        Process p2 = runMongodInstance(27020);
-        //p.destroy();
-        //p1.destroy();
-        //p2.destroy();
+       MongodInstanceBuilder builder = new MongodInstanceBuilder();
+        builder.runMongod(27020);
+        builder.showMapEntries();
+        builder.runMongod(27020);
+        builder.showMapEntries();
+        builder.stopMongod(27020);
+        builder.showMapEntries();
+
     }*/
 
 }
