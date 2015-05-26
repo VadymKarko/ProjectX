c47aec893c47bd6a7bf0f4799588988e1c9f9ff2
VadymKarko <vadym.karko@gmail.com>
2015-05-26 18:50:49 -0400

    Glassfish Process builder

--- repository/src/main/java/projectx/repository/connectors/GlassfishBuilder.java
+++ repository/src/main/java/projectx/repository/connectors/GlassfishBuilder.java
@@ -1,6 +1,8 @@
 package projectx.repository.connectors;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * @author proger
@@ -10,35 +12,59 @@ public class GlassfishBuilder {
 
     private String domain ="domain1";
     private String cluster = "cluster1";
-    private String server = "server";
+   // private String server = "server";
     private String user = "admin";
-    private String name = "~/ProjectX/web/target/web.war";
-    private String node1 = "http://localhost:28080/web/index.xhtml";
-    private String node2 = "http://localhost:28081/web/index.xhtml";
-
-    public void runGlassfishInstanceCommand (String parametr) throws IOException,
+   // private String name = "~/ProjectX/web/target/web.war";
+    private String passwordFilePath = "/home/proger/password.txt";
+    private String script="--user "+ user +" --passwordfile " + passwordFilePath +" ";
+    /*
+    Create password.txt file in /home/proger/ with following
+        AS_ADMIN_PASSWORD=adminadmin
+        AS_ADMIN_ADMINPASSWORD=adminadmin
+        AS_ADMIN_USERPASSWORD=adminadmin
+        AS_ADMIN_MASTERPASSWORD=adminadmin
+     */
+    public void runServerInstanceCommand (String parametr) throws IOException,
             InterruptedException, IllegalArgumentException {
-        String script;
-        if (parametr.equals("deploy")){script=parametr + " --target "+ cluster + " --user " +
-                                        user + " --name web " + name;}
-        else if (parametr.equals("redeploy")){script=parametr + " --target "+ cluster + " --user " +
-                user + " --name web " + name;}
-        else if (parametr.equals("undeploy")){script=parametr + " --target "+ server + " --user " +
-                user + " web ";}
-        else if (parametr.equals("start-domain")){script=parametr +" "+ domain;}
-        else if (parametr.equals("stop-domain")){script=parametr +" "+ domain;}
-        else {throw  new IllegalArgumentException("Invalid command");}
-        String[] command = {"bash","-c", "echo proger| sudo -S asadmin " + script };
-        Runtime.getRuntime().exec(command);
+        String[] command = {"bash","-c", "asadmin " + parametr+"-domain " + domain };
+        Process pb =Runtime.getRuntime().exec(command);
+        printOutput(pb);
+        pb.waitFor();
     }
-    public void runNodeInstanceCommand(String parametr,String node) throws IOException,
+     public void runNodeInstanceCommand(String parameter,String node) throws IOException,
             InterruptedException, IllegalArgumentException{
-        String script;
-        if (parametr.equals("start")){script=parametr + "-instance "+ node;}
-        else if (parametr.equals("stop")){script=parametr + "-instance "+ node;}
-        else {throw  new IllegalArgumentException("Invalid command");}
-        String[] command = {"bash","-c", "echo proger| sudo -S asadmin " + script };
-        Runtime.getRuntime().exec(command);
+        String[] command = {"bash","-c", "asadmin " + script  + parameter +"-instance " + node};
+        Process pb =Runtime.getRuntime().exec(command);
+         printOutput(pb);
+         pb.waitFor();
+    }
+    public void runClusterInstanceCommand(String parameter) throws IOException,
+            InterruptedException, IllegalArgumentException{
+        String[] command = {"bash","-c", "asadmin " + script + parameter + "-cluster " + cluster};
+        System.out.println("asadmin " + script);
+        Process pb =Runtime.getRuntime().exec(command);
+        printOutput(pb);
+        pb.waitFor();
 
     }
+    public static void printOutput(Process pb) throws IOException{
+    String line;
+    BufferedReader input = new BufferedReader(new InputStreamReader(pb.getInputStream()));
+
+    while ((line = input.readLine()) != null) {
+
+        System.out.println(line);
+    }
+    input.close();}
+/*
+public static void main(String[] args)
+            throws IOException, InterruptedException, IllegalArgumentException
+    {
+       // GlassfishBuilder builder = new GlassfishBuilder();
+      //builder.runServerInstanceCommand("start");
+      //builder.runClusterInstanceCommand("start");
+        //builder.runNodeInstanceCommand("stop","node1");
+
+
+    }*/
 }
